66317a85316d74a90474b2f0525a1838799f9ac1
Andrii Borovyi <aborovyi@llnw.com>
2021-01-04 01:31:12 +0200

    Completed day 07, part 1

---
+++ year2020/day07/handy_haversacks_p1.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+import re
+from collections import namedtuple
+
+
+Bag = namedtuple("Bag", ["amount", "colour"])
+
+
+def extract_bags(line):
+    bags_outer_exp = r"(?P<outer>.+) bags contain (?P<inner>.+)"
+    bags_inner_exp = r"(?P<amount>\d+) (?P<colour>\D+) bag"
+    bags = {}
+
+    outer = re.search(bags_outer_exp, line)
+
+    if outer is not None:
+        outer_bag = outer.groupdict().get("outer")
+        bags[outer_bag] = {}
+        inner = re.findall(bags_inner_exp, outer.groupdict().get("inner"))
+        if inner is not None:
+            for itm in inner:
+                bag = Bag(*itm)
+                bags[outer_bag][bag.colour] = int(bag.amount)
+
+    return bags
+
+
+def update_bags(existing={}, incoming={}):
+    for outer, inner in incoming.items():
+        if outer not in existing.keys():
+            existing[outer] = inner
+        else:
+            existing[outer].update(inner)
+    return existing
+
+
+def build_dependencies(source={}, target=""):
+    outer_bags = []
+    added_more = True
+    for outer, inner in source.items():
+        if target in inner.keys():
+            outer_bags.append([target, outer])
+
+    while added_more:
+        added_more = False
+        new_outer = []
+        for bag_list in outer_bags:
+            last_bag = bag_list[-1]
+            last_bag_found = False
+
+            for outer_bag, inner_bags in source.items():
+                if last_bag in inner_bags.keys():
+                    last_bag_found = True
+                    new_list = list(bag_list)
+                    new_list.append(outer_bag)
+                    new_outer.append(new_list)
+                    added_more = True
+
+            if last_bag_found is False:
+                new_outer.append(bag_list)
+
+        if new_outer:
+            outer_bags = list(new_outer)
+
+    return outer_bags
+
+
+def solve_the_task(filename=None, target_color=None):
+    collected_bags = {}
+    collected_rules = 0
+
+    with open(filename, "r") as f:
+        for line in f:
+            collected_rules += 1
+            line = line.strip()
+            bags_from_file = extract_bags(line)
+            collected_bags = update_bags(collected_bags, bags_from_file)
+
+    bags_dependencies = build_dependencies(collected_bags, target_color)
+    outer_bags = set()
+    for bag_list in bags_dependencies:
+        for bag in bag_list:
+            outer_bags.add(bag)
+
+    return len(outer_bags) - 1  # Except of the bag itself
+
+
+if __name__ == '__main__':
+    target_color = "shiny gold"
+    bags = solve_the_task(filename="input_data.txt", target_color=target_color)
+    print(f"{target_color} resides in {bags} bags.")

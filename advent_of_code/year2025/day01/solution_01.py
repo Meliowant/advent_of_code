8044c8788f41475f59102f3b1deedeb5fcc1c373
Andrii Borovyi <andrii.borovyi@quantum.com>
2025-12-06 19:12:40 +0200

    Day 1: Task 1 is completed

--- advent_of_code/year2025/day01/solution_01.py
+++ advent_of_code/year2025/day01/solution_01.py
@@ -2,6 +2,70 @@
 """
 Solution for day 1, task 1.
 """
+import argparse
 
-locker_wheel = range(100)
+class SafeDial:
+    """ This is safe dial class to pick correct password to enter throuh
+    the secret entrance at the North Pole
+    """
+    def __init__(self, size: int, init_position: int = 0):
+        self.size = size
+        self._position = init_position
 
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def set_position(self, value):
+        self._position = value
+
+    def rotate_left(self, shift):
+        self._position = self._position - shift
+        if self._position < 0:
+            self._position = self.size + self._position
+
+    def rotate_right(self, shift):
+        self._position = self._position + shift
+        if self._position >= self.size:
+            self._position = self._position - self.size
+
+    def rotate(self, value):
+        """Take decision where to turn the dial, and update the position
+           accordingly"""
+
+        direction, step = value[:1], int(value[1:])
+        step = step % self.size
+        if direction == "R":
+            self.rotate_right(step)
+        elif direction == "L":
+            self.rotate_left(step)
+
+def solve_task_01(input_file):
+    """Code to run first solution for day 1"""
+
+    sd = SafeDial(100, 50)
+    zero_tick_counter = 0
+    positions = []
+
+    with open(input_file, "rt") as fd:
+        line = fd.readline().strip()
+
+        while line:
+            sd.rotate(line)
+            positions.append(sd.position)
+            line = fd.readline().strip()
+
+    print(
+        f"Dial was at 0 position {positions.count(0)} times "
+        "for {len(positions)} turns."
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            "-f", "--file", help="File to read input from", required=True
+    )
+    args = parser.parse_args()
+    solve_task_01(args.file)
--- tests/year_2025/day_01/test_solution_01.py
+++ tests/year_2025/day_01/test_solution_01.py
@@ -1,10 +1,109 @@
 #!/usr/bin/env python3
 import pytest
 
-from advent_of_code.year2025.day01.solution_01 import locker_wheel
+from advent_of_code.year2025.day01.solution_01 import SafeDial
 
-def test_locker_wheel():
+def test_safedial_init():
     """Check if wheel contains all values"""
-    assert locker_wheel[0] == 0
-    assert locker_wheel[-1] == 99
+    sd = SafeDial(10)
+    assert sd.size == 10
 
+@pytest.mark.parametrize(
+    "dial_size", [
+        1, 5, 15
+    ]
+)
+def test_current_position(dial_size):
+    """Check position for the dial"""
+    sd = SafeDial(dial_size)
+    assert sd.position == 0
+
+
+@pytest.mark.parametrize("init_pos", [1, 2, 4, 5, 7])
+def test_safedial_init_pos(init_pos):
+    """Check if user-defined initial position is OK"""
+    sd = SafeDial(10, init_pos)
+    assert sd.position == init_pos
+
+@pytest.mark.parametrize(
+    "dial_size, init_pos, shift, new_pos",
+    [
+        (10, 9, 1, 8),
+        (10, 5, 5, 0),
+        (10, 9, 11, 8),
+        (10, 0, 1, 9),
+        (100, 19, 19, 0),
+        (100, 5, 10, 95)
+    ]
+)
+def test_rotate_left(dial_size, init_pos, shift, new_pos):
+    """Check if rotation left works as expected"""
+    sd = SafeDial(dial_size, init_pos)
+    sd.rotate_left(shift)
+    assert sd.position == new_pos
+
+@pytest.mark.parametrize(
+    "dial_size, init_pos, shift, new_pos",
+    [
+        (10, 9, 1, 0),
+        (10, 5, 5, 0),
+        (10, 9, 10, 9),
+        (100, 11, 8, 19),
+        (100, 99, 1, 0),
+    ]
+)
+def test_rotate_right(dial_size, init_pos, shift, new_pos):
+    """Check if rotation left works as expected"""
+    sd = SafeDial(dial_size, init_pos)
+    sd.rotate_right(shift)
+    assert sd.position == new_pos
+
+@pytest.mark.parametrize(
+        "dial_size, rot_str, exp_val", [
+            (10, ("R5", ), 5),
+            (2, ("R1", "L1", ), 0),
+            (2, ("L1", "R1", ), 0),
+            (100, ("L1", ), 99),
+        ],
+        ids=[
+            "10-(R5)-5",
+            "2-(R1-L1)-0",
+            "2-(L1-R1)-0",
+            "100-(L1)-0",
+            ]
+)
+def test_rotation(dial_size, rot_str, exp_val):
+    """ Test rotation algorithm """
+    sd = SafeDial(dial_size)
+    for r in rot_str:
+        sd.rotate(r)
+    assert sd.position == exp_val
+
+
+def test_acceptance():
+    """
+    Example, provided by Advent of Code.
+    Safe dial's size is 99. Initial value - 0.
+    Turns are:
+    - L68
+    - L30
+    - R48
+    - L5
+    - R60
+    - L55
+    - L1
+    - L99
+    - R14
+    - L82
+    """
+
+    turns = [
+            "L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"
+    ]
+    sd = SafeDial(100, 50)
+    positions = []
+    for turn in turns:
+        sd.rotate(turn)
+        if sd.position == 0:
+            positions.append(sd.position)
+    assert len(positions) == 3, positions
